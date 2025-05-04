08a22b6c0497ea6f9efbea7976537cd104333d3c
Chevi <a@gmail.com>
2025-05-04 14:54:06 +0300

    f

--- src/components/home/homeAbout.jsx
+++ src/components/home/homeAbout.jsx
@@ -194,12 +194,12 @@ export const HomeAbout = () => {
       <Box className="hero-section">
         {/* Video Background with Overlay */}
         <Box className="video-background">
-          <Box
+          {/* <Box
             component="img"
             src="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80"
             alt="Travel video background"
             className="background-image"
-          />
+          /> */}
           <IconButton 
             className="video-control-button"
             onClick={() => setIsVideoPlaying(!isVideoPlaying)}
